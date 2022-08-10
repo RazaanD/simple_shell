64ff27819e636b9045bfb56bdeb0250ca414a56c
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-08-10 22:22:01 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+ * main - Initializes the program
+ * Return: 0 Success
+ */
+int main(void)
+{
+	if (isatty(STDIN_FILENO))
+	{
+		shell();
+	}
+	else
+	{
+		shell_direc();
+	}
+	return (0);
+}
+/**
+ * shell - interprets the command line in interactive mode
+ * Return: Void
+ */
+void shell(void)
+{
+	int state = 0;
+
+	while (1)
+	{
+		write(1, "shell $ ", 8);
+		state = shell_direc();
+
+		if (state != 0)
+		{
+			continue;
+		}
+	}
+}
