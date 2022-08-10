971f9675f7f2923bf307b6c8df5efaff4a80da46
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-08-10 22:19:27 +0200

    Create argu.c

---
+++ argu.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/**
+ * argument - Read line command
+ * Return: Address of the string with tokenized arguments
+ */
+char **argument(void)
+{
+	size_t len_arg = 0;
+	char *buff_arg = NULL;
+	char **arg_token = NULL;
+
+	if (getline(&buff_arg, &len_arg, stdin) != EOF)
+	{
+		if (buff_arg[0] == 10)
+		{
+			free(buff_arg);
+			return (NULL);
+		}
+		arg_token = tock_arg(buff_arg);
+		free(buff_arg);
+	}
+	else
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		free(buff_arg);
+		exit(0);
+	}
+	return (arg_token);
+}
