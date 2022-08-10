71ff34e17cd5879a7aec2114e1b7544090da872d
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-08-10 22:23:56 +0200

    Create shell_direc.c

---
+++ shell_direc.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+/**
+ * shell_direc - Interprets commands in non-interactive mode
+ * Return: 0 Success
+ */
+int shell_direc(void)
+{
+	char **arg_token = NULL;
+	struct stat buff;
+	char *command = NULL;
+
+	arg_token = argument();
+
+	if (!arg_token)
+		return (-1);
+
+	if (stat(arg_token[0], &buff) == 0)
+	{
+		command = _strdup(arg_token[0]);
+		execute(command, arg_token);
+		free(command);
+		free_doble_pointer(arg_token);
+	}
+	else if (stat(arg_token[0], &buff) == -1)
+	{
+		command = get_command(arg_token);
+		if (command != NULL)
+		{
+			execute(command, arg_token);
+			free(command);
+			free_doble_pointer(arg_token);
+		}
+		else
+		{
+			perror("Error");
+			free_doble_pointer(arg_token);
+			return (-1);
+		}
+	}
+	else
+	{
+		perror("Error");
+		return (-1);
+	}
+	return (0);
+}
+
+/**
+ * get_command - add the path to the command and check if it is executable
+ * @arg_token: Arguments tokenized.
+ * Return: Funtion
+ */
+char *get_command(char **arg_token)
+{
+	char *path = NULL;
+	char **path_token = NULL;
+
+	path = get_path();
+	path_token = token_path(path);
+
+	return (check_executable(path_token, arg_token));
+}
